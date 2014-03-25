fb3a5f9e897f9d44a2333048a3078ba9cc4a5d52
Phil <inbox@philipjun.com>
2014-03-25 15:12:13 -0400

    exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,23 @@
+1.upto(100) do |n|
+
+if 		n % 3 == 0 &&  n % 5 ==0
+		puts "Bitmaker"
+
+	elsif 
+	 	 n % 3 == 0
+		puts "Bit"
+	
+	elsif
+	 	 n % 5 ==0
+		puts "Maker"
+
+	
+	else
+		puts n
+		
+	end
+
+end
+
+
+
