f946c1843c9d8e138a1a726e4e5ba71c60b1dd12
Phil <inbox@philipjun.com>
2014-03-25 13:56:04 -0400

    2

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+a = "10".to_s
+b = "0.2".to_s
+
+
+puts "The tip costs #{a + b} for a 55 dollar meal"
+
+c = 45628
+d = 7839
+
+puts "#{c * d}"
+
+puts (true && false) || (false && true) || !(false && false)
